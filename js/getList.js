6b534e65d4263a57830529d0c00e61d2249dd1f8
user <123@123.com>
2023-12-11 17:41:31 +0800

    新增查询价格区间，有小问题暂时不管

--- js/getList.js
+++ js/getList.js
@@ -1,5 +1,5 @@
 let model =
-            `
+    `
                         <div class="item">
                             <img src="./images/01.webp" alt="" width="100%" style="margin-top: 10px;">
                             <div class="title">
@@ -19,37 +19,34 @@ let model =
                         </div>
                     `
 
-        function getList(page) {
-            getProductions(page,
-                (e, d) => {
-                    document.getElementById('mainContain').innerHTML = "";
-                    d.forEach(() => {
-                        document.getElementById('mainContain').innerHTML += model
-                    })
+function getList(data) {
 
-                    items = document.querySelectorAll(".item");
-                    titleElement = document.querySelectorAll(".title");
-                    price = document.querySelectorAll(".price");
-                    store = document.querySelectorAll(".store");
-                    sales = document.querySelectorAll(".sales");
+        document.getElementById('mainContain').innerHTML = "";
+        data.forEach(() => {
+            document.getElementById('mainContain').innerHTML += model
+        })
 
-                    for (let index = 0; index < items.length; index++) {
-                        items[index].querySelector("img").src = d[index].imageUrl;
-                    }
-                    for (let index = 0; index < titleElement.length; index++) {
-                        titleElement[index].querySelector("span").innerHTML = d[index].name;
-                    }
-                    for (let index = 0; index < price.length; index++) {
-                        price[index].querySelectorAll("span")[0].innerHTML = "￥" + d[index].nowPrice;
-                        price[index].querySelectorAll("span")[1].innerHTML = "￥" + d[index].oldPrice;
-                    }
-                    for (let index = 0; index < store.length; index++) {
-                        store[index].querySelector("span").innerHTML = d[index].storeName;
-                    }
-                    for (let index = 0; index < sales.length; index++) {
-                        sales[index].querySelector("div").innerHTML = "月销" + d[index].sales;
-                    }
+        items = document.querySelectorAll(".item");
+        titleElement = document.querySelectorAll(".title");
+        price = document.querySelectorAll(".price");
+        store = document.querySelectorAll(".store");
+        sales = document.querySelectorAll(".sales");
 
-                }
-            );
+        for (let index = 0; index < items.length; index++) {
+            items[index].querySelector("img").src = data[index].imageUrl;
         }
+        for (let index = 0; index < titleElement.length; index++) {
+            titleElement[index].querySelector("span").innerHTML = data[index].name;
+        }
+        for (let index = 0; index < price.length; index++) {
+            price[index].querySelectorAll("span")[0].innerHTML = "￥" + data[index].nowPrice;
+            price[index].querySelectorAll("span")[1].innerHTML = "￥" + data[index].oldPrice;
+        }
+        for (let index = 0; index < store.length; index++) {
+            store[index].querySelector("span").innerHTML = data[index].storeName;
+        }
+        for (let index = 0; index < sales.length; index++) {
+            sales[index].querySelector("div").innerHTML = "月销" + data[index].sales;
+        }
+
+}
