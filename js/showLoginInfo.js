8bcbe68d0ed32d2966ba07b8d23dc34ff3567877
user <123@123.com>
2023-12-29 19:28:57 +0800

    修修改改缝缝补补

--- js/showLoginInfo.js
+++ js/showLoginInfo.js
@@ -1,22 +1,24 @@
 // 获取 Cookie
 let username = $.cookie('username');
 let password = $.cookie('password');
-
 // 检查 Cookie 是否存在
-if (username) {
-    $(document).ready(function () {
-        if (username) {
-            getUserInfo(
-                (e, d) => {
-                    d.forEach(element => {
-                        if (element.username == username && element.password == password) {
-                            $("#login-info").text(element.username);
-                            $("#login-info").attr("href","#");
-                        }
-                    });
-                }
-            );
 
-        }
-    });
-}
+$(document).ready(function () {
+    if (username) {
+        getUserInfo(
+            (e, d) => {
+                d.forEach(element => {
+                    if (element.username == username && element.password == password) {
+                        $("#login-info").text(element.username);
+                        $("#login-info").attr("href", "#");
+                    }
+                });
+            }
+        );
+
+    }else if ( localStorage.getItem('username') != null) {
+        let localUsername = localStorage.getItem('username')
+        $("#login-info").text('临时用户 ' + localUsername);
+        $("#login-info").attr("href", "#");
+    }
+});
